f3e2bd3f830a79c0b5e7d8609c1cd429d9c35b8e
EldarSalmanow <eldar.salm@gmail.com>
2026-03-27 00:28:54 +0300

    Some fixes in Treap

--- TreeDataStructures/Implementations/Treap/Treap.cs
+++ TreeDataStructures/Implementations/Treap/Treap.cs
@@ -5,42 +5,44 @@ namespace TreeDataStructures.Implementations.Treap;
 public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<TKey, TValue>>
     where TKey : IComparable<TKey>
 {
-    protected override TreapNode<TKey, TValue> CreateNode(TKey key, TValue value)
-        => new(key, value);
+    protected override TreapNode<TKey, TValue> CreateNode(TKey key, TValue value) => 
+        new(key, value);
 
     protected virtual (TreapNode<TKey, TValue>?, TreapNode<TKey, TValue>?)
         Split(TreapNode<TKey, TValue>? node, TKey key)
     {
         if (node is null) return (null, null);
 
-        var cmp = Comparer.Compare(node.Key, key);
-
-        if (cmp <= 0)
-        {
-            var (left, right) = Split(node.Right, key);
-            
-            node.Right = left;
-            left?.Parent = node;
-            node.Parent = null;
-            right?.Parent = null;
-            
-            return (node, right);
-        } 
-        else 
-        {
-            var (left, right) = Split(node.Left, key);
-            
-            node.Left = right;
-            right?.Parent = node;
-            node.Parent = null;
-            left?.Parent = null;
-            
-            return (left, node);
-        }
+        return Comparer.Compare(node.Key, key) <= 0
+            ? SplitLeft(node, key)
+            : SplitRight(node, key);
     }
 
-    protected virtual TreapNode<TKey, TValue>? Merge(
-        TreapNode<TKey, TValue>? left, TreapNode<TKey, TValue>? right)
+    private (TreapNode<TKey, TValue>?, TreapNode<TKey, TValue>?) SplitLeft(TreapNode<TKey, TValue> node, TKey key)
+    {
+        var (left, right) = Split(node.Right, key);
+
+        node.Right = left;
+        left?.Parent = node;
+        node.Parent = null;
+        right?.Parent = null;
+
+        return (node, right);
+    }
+
+    private (TreapNode<TKey, TValue>?, TreapNode<TKey, TValue>?) SplitRight(TreapNode<TKey, TValue> node, TKey key)
+    {
+        var (left, right) = Split(node.Left, key);
+
+        node.Left = right;
+        right?.Parent = node;
+        node.Parent = null;
+        left?.Parent = null;
+
+        return (left, node);
+    }
+
+    protected virtual TreapNode<TKey, TValue>? Merge(TreapNode<TKey, TValue>? left, TreapNode<TKey, TValue>? right)
     {
         if (left is null)
         {
@@ -66,88 +68,79 @@ public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<
             
             return left;
         }
-        else
-        {
-            right.Left = Merge(left, right.Left);
 
-            if (right.Left is { } rightLeft) rightLeft.Parent = right;
+        right.Left = Merge(left, right.Left);
 
-            right.Parent = null;
-
-            return right;
-        }
+        if (right.Left is { } rightLeft) rightLeft.Parent = right;
+        
+        right.Parent = null;
+        
+        return right;
     }
+
 
     public override void Add(TKey key, TValue value)
     {
         if (FindNode(key) is { } existing)
         {
             existing.Value = value;
-            
+
             return;
         }
 
         var node = CreateNode(key, value);
-        
+
         var (left, right) = Split(Root, key);
-        
+
         Root = Merge(Merge(left, node), right);
         Count++;
-        
+
         OnNodeAdded(node);
     }
 
     public override bool Remove(TKey key)
     {
-        TreapNode<TKey, TValue>? removed = null;
-        TreapNode<TKey, TValue>? rebalanceParent = null;
-        TreapNode<TKey, TValue>? replacement = null;
+        TreapNode<TKey, TValue>? current = Root, parent = null;
 
-        TreapNode<TKey, TValue>? Erase(TreapNode<TKey, TValue>? node)
+        while (current != null)
         {
-            if (node is null) return null;
+            var cmp = Comparer.Compare(key, current.Key);
 
-            var cmp = Comparer.Compare(key, node.Key);
-
-            switch (cmp)
+            if (cmp == 0)
             {
-                case < 0:
-                {
-                    node.Left = Erase(node.Left);
-                    if (node.Left is { } left) left.Parent = node;
-                    node.Parent = null;
+                var replacement = Merge(current.Left, current.Right);
 
-                    return node;
-                }
-                case > 0:
+                if (parent is null)
                 {
-                    node.Right = Erase(node.Right);
-                    if (node.Right is { } right) right.Parent = node;
-                    node.Parent = null;
-
-                    return node;
+                    Root = replacement;
                 }
+                else if (current == parent.Left)
+                {
+                    parent.Left = replacement;
+                }
+                else
+                {
+                    parent.Right = replacement;
+                }
+
+                replacement?.Parent = parent;
+
+                current.Left = null;
+                current.Right = null;
+                current.Parent = null;
+
+                Count--;
+                
+                OnNodeRemoved(parent, replacement);
+
+                return true;
             }
 
-            removed = node;
-            rebalanceParent = node.Parent;
-            replacement = Merge(node.Left, node.Right);
-
-            node.Left = null;
-            node.Right = null;
-            node.Parent = null;
-
-            return replacement;
+            parent = current;
+            
+            current = cmp < 0 ? current.Left : current.Right;
         }
 
-        Root = Erase(Root);
-
-        if (removed is null) return false;
-
-        Count--;
-        
-        OnNodeRemoved(rebalanceParent, replacement);
-        
-        return true;
+        return false;
     }
 }
